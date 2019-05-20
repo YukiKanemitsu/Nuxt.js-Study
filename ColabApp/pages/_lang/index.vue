fa5e7bc6afceb66caceeaffdbc77a6463230551f
YukiKanemitsu <y.kanemitsu0723@gmail.com>
2019-05-21 00:32:25 +0900

    Update_ColabApp

--- ColabApp/pages/_lang/index.vue
+++ ColabApp/pages/_lang/index.vue
@@ -1,24 +1,22 @@
 <template>
   <section class="container">
     <div>
-      <logo />
+      <logo/>
       <h1 class="title">
-        ColabApp
+        {{ $t('home.title') }} 
       </h1>
       <h2 class="subtitle">
-        My majestic Nuxt.js project
+        {{ $t('home.introduction') }} 
       </h2>
       <div class="links">
         <a
           href="https://nuxtjs.org/"
           target="_blank"
-          class="button--green"
-        >Documentation</a>
+          class="button--green">Documentation</a>
         <a
           href="https://github.com/nuxt/nuxt.js"
           target="_blank"
-          class="button--grey"
-        >GitHub</a>
+          class="button--grey">GitHub</a>
       </div>
     </div>
   </section>
